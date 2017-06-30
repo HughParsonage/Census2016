e972c90664ea1f0671445dadc2990e729595e6e4
HughParsonage <hugh.parsonage@gmail.com>
2017-06-30 11:08:12 +1000

    Replace zero SA2s with NAs; LazyData

--- R/Census2016_wide_by_SA2_year.R
+++ R/Census2016_wide_by_SA2_year.R
@@ -1,5 +1,5 @@
 #' Census data by SA2
-#' 
+#' @details If no data is offered by the ABS for that SA2, the data is marked as missing  and the column \code{isMissing} is \code{TRUE}.
 
 "Census2016_wide_by_SA2_year"
 
