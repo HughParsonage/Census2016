cdbe71c8ab5a71f27d26e07f053fbecaf4e7d4bc
HughParsonage <hugh.parsonage@gmail.com>
2017-07-15 21:25:28 +1000

    May this package name be appropriate ..

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname = find.package("Census2016"), pkgname = "Census2016") {
+  packageStartupMessage("For additional data packages for the 2016 Census, visit https://github.com/HughParsonage/Census2016.DataPack")
+}
+
